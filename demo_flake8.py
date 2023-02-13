46d8d77d6ac6e9de0017c32a6a727b475b2e1d08
fermine <ferminehelene@MacBook-Air-de-fermine.local>
2023-02-13 13:34:44 +0100

    Initial commit

---
+++ demo_flake8.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+print(
+    "hello world hello world hello world hello world hello world hello world hello world hello world hello world"
+)
+package_dir = Path(__file__).parent.absolute()
+print(package_dir)
